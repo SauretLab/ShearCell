6ca16515be544e48048968015932510389b3065f
raaghavt <96094982+raaghav-t@users.noreply.github.com>
2023-09-27 12:55:56 -0700

    plot

--- PlotScript/plotData.m
+++ PlotScript/plotData.m
@@ -1,11 +1,11 @@
-list = {'Max','11kg','8kg','6kg','4kg','0kg'};
-
+%list = {'Max','11kg','8kg','6kg','4kg','0kg'};
+list = {'0kg'};
 for i = 1:length(list)
     filename = strcat('firstCohesive',string(list(i)),'.csv');
     a = readmatrix(filename);
 
     plot(a(1,:),a(2,:));
-    xlim([0 200]);
+    xlim([0 inf]);
     
     hold on
 end
---
+++ PlotScript/plotPoints.m
@@ -0,0 +1,4 @@
+load('var.mat');
+scatter(var(1,:), var(2,:)/0.02039);
+xlabel('Normal Stress')
+ylabel('Shear Stress')
