1866207aac9ab042f76cfaa863160c744f9d4a8d
raaghavt <96094982+raaghav-t@users.noreply.github.com>
2023-09-27 11:33:26 -0700

    Update plotData.m

--- PlotScript/plotData.m
+++ PlotScript/plotData.m
@@ -1,9 +1,13 @@
-list = ['Max','11kg','8kg','6kg','4kg','0kg'];
+list = {'Max','11kg','8kg','6kg','4kg','0kg'};
 
-for i = 1:size(list)
-    filename = strcat('firstCohesive',num2str(stations(i)),'.csv')
+for i = 1:length(list)
+    filename = strcat('firstCohesive',string(list(i)),'.csv');
     a = readmatrix(filename);
 
     plot(a(1,:),a(2,:));
-
+    xlim([0 200]);
+    
+    hold on
 end
+
+legend(string(list))
